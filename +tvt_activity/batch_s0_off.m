7cb62cb43bc574d154fabfa98d26e618f2f10984
andreat <andrea.tassi@gmail.com>
2017-06-15 13:27:06 +0100

    Third release of the framewrok.

--- +tvt_activity/batch_s0_off.m
+++ +tvt_activity/batch_s0_off.m
@@ -269,51 +269,6 @@ tvt_activity.stochasticLib.scenarioRunner
 baseName = '+tvt_activity/data/s_0_off';
 save(strcat(baseName,'/rate_Validation_30_180_2_B.mat'), 'P_rate_coverage_lb', 'P_rate_coverage_sim');
 
-
-%% Two Obstacle Lanes per-Driving Direction (Assoc. Prob)
-clearvars;
-alpha_nlos = 5.76;
-userYoff = -3.7/2;
-simSection = true;
-getTh = true;
-recoverRate = false;
-getAssocProbs = true;
-no_obs_lanes   = 2;
-antenna_sector = deg2rad(30);
-b_span         = deg2rad(180);
-no_obs         = [2e2, 1e2];
-road_len       = 1e4;
-txAntenna_maxGain  = tvt_activity.utils.db2Lin(20);
-txAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
-rxAntenna_maxGain  = tvt_activity.utils.db2Lin(10);
-rxAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
-tvt_activity.stochasticLib.scenarioRunner
-baseName = '+tvt_activity/data/s_0_off';
-save(strcat(baseName,'/assoc_Validation_30_180_2_A.mat'), 'served_los_count_sim', 'served_los_count_th');
-
-%% ZOOM In
-% Two Obstacle Lanes per-Driving Direction (Assoc. Prob)
-clearvars;
-alpha_nlos = 5.76;
-no_bs    = 2:1:10;
-userYoff = -3.7/2;
-simSection = true;
-getTh = true;
-recoverRate = false;
-getAssocProbs = true;
-no_obs_lanes   = 2;
-antenna_sector = deg2rad(30);
-b_span         = deg2rad(180);
-no_obs         = [2e2, 1e2];
-road_len       = 1e4;
-txAntenna_maxGain  = tvt_activity.utils.db2Lin(20);
-txAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
-rxAntenna_maxGain  = tvt_activity.utils.db2Lin(10);
-rxAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
-tvt_activity.stochasticLib.scenarioRunner
-baseName = '+tvt_activity/data/s_0_off';
-save(strcat(baseName,'/assoc_Validation_30_180_2_A_zoom.mat'), 'served_los_count_sim', 'served_los_count_th');
-
 %% One Obs. Lane (p_L)
 clearvars;
 simPl = true;
@@ -337,7 +292,6 @@ rxAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
 tvt_activity.stochasticLib.scenarioRunner_pL
 baseName = '+tvt_activity/data/s_Inf';
 save(strcat(baseName,'/pL_30_180_1_A.mat'));
-%system('cp -r +tvt_activity/data /home/at15088/Dropbox/UoB')
 
 % Two Obs. Lanes (p_L)
 clearvars;
@@ -362,4 +316,3 @@ rxAntenna_minGain  = tvt_activity.utils.db2Lin(-10);
 tvt_activity.stochasticLib.scenarioRunner_pL
 baseName = '+tvt_activity/data/s_Inf';
 save(strcat(baseName,'/pL_30_180_2_A.mat'));
-%system('cp -r +tvt_activity/data /home/at15088/Dropbox/UoB')
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # V2X-MMWAVE
 
+For further details, please refer to https://arxiv.org/abs/1706.00298
+
 License
 -------
-At this stage, the code is made available only to the reviewers of the manuscript. The code will be made publicly available when the manuscript will be published. As such, at this stage, the code cannot be redistributed.
-
 Please note that the code contained in
     - ./src/export_fig
     - ./src/+tvt_activity/+utils/plotsparsemarkers.m
@@ -20,10 +20,8 @@ Furthermore, the code requires what follows:
     - MATLAB R2015a (including the Parallel Computing Toolbox) with the capability of building MEX files by means of g++. Please be aware that our MEX files are compiled by using the g++ compiler flag --std=c++11.
 
 
-To Replicate the results and the figures reported in the manuscript and in the reply to the reviewers:
+To Replicate the results and the figures reported in the manuscript:
 -------------------------------------------------------------
     0. Add to the MATLAB path the folder ./src/export_fig
     1. Change the MATLAB folder to ./src
     2. Run in MATLAB the script main.m - This will both generate the results (in ./src/data) and all the figures included both in the manuscript and in the reply to the reviewers (in ./src/doc/Img). 
-
-**Please note that both the results and figures have been already included in the aforementioned folders.**
